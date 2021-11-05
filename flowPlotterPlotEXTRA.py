6cb23fc764fbcb35d826bb97e3d69a37c98cac38
Starkkad <abilevich@itba.edu.ar>
2021-11-05 10:40:41 -0300

    asd

--- flowPlotterPlotEXTRA.py
+++ flowPlotterPlotEXTRA.py
@@ -12,14 +12,14 @@ fileName2 = -1
 fileName3 = -1
 	
 def plotFlow():
-	group = ['d=1.2,N=200','d=1.8,N=260','d=2.4,N=320','d=3.0,N=380']
+	group = ['1.2','1.8','2.4','3.0']
 	data1 = [3.07, 4.43, 5.58, 6.91]
 	errors = [0.46, 0.65, 0.66, 0.71]
 
 	plt.errorbar(group, data1, yerr=errors, fmt='-o')
 	plt.grid(b=True, which='both', axis='both')
 	plt.ylabel('Caudal Estacionario Promedio (particulas/s)')
-	plt.xlabel('Caso')
+	plt.xlabel('Tamaño de Apertura (m)')
 	plt.show()
 
 	return
